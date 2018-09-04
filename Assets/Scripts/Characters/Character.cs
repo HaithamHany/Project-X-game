6462f0658df14ff1a04a4f54d0980d506a2698e6
Haitham Abdel-Salam <haithamhany7@gmail.com>
2018-09-04 07:52:25 +0200

    adding jumping animation. To be implemented next commit

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -2,38 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Oriantation
-{
-left,right
-}
-enum PlayerType
-{
-    Player1,Player2,NPC
-}
-
+//this class should be Abstract class only
 public class Character : MonoBehaviour {
+    public enum Oriantation
+    {
+        left, right
+    }
 
     [SerializeField]
     private float _speed;
-    private float _power;
+    [SerializeField]private float _jumpPower;
     private float _health;
     private Animator _anim;
     private Rigidbody _characterRigBody;
     private GameObject _opponent;
-    private PlayerType _player_type;
-    private Oriantation _oriantation;
-    [SerializeField][Range(0.01f,0.09f)]
-    private float _rotationSpeed= 0.01f;
+
+    //for jumping
+    private bool _Grounded=false;
+    [SerializeField] private LayerMask _WhatIsGround;
+    const float _GroundedRadius = .2f;
+
+    [SerializeField][Range(0.01f,0.15f)]protected float _rotationSpeed= 0.08f;
+    protected Oriantation _oriantation;
 
     public float Speed
     {
         get { return _speed; }
         set { _speed = value; }
     }
-    public float Power
+    public float JumpPower
     {
-        get { return _power; }
-        set { _power = value; }
+        get { return _jumpPower; }
+        set { _jumpPower = value; }
     }
     public float Health
     {
@@ -42,9 +42,8 @@ public class Character : MonoBehaviour {
     }
 
 
-    protected virtual void Start () {
+    protected virtual void Awake () {
 
-        _player_type = PlayerType.Player1;
         _oriantation = Oriantation.left;
         _anim = GetComponent<Animator>();
         _characterRigBody = GetComponent<Rigidbody>();
@@ -60,35 +59,42 @@ public class Character : MonoBehaviour {
         if (_oriantation == Oriantation.left)
             _anim.SetFloat("Velocity", _characterRigBody.velocity.x);
         else
-            _anim.SetFloat("Velocity", -1*(_characterRigBody.velocity.x)); 
+            _anim.SetFloat("Velocity", -1*(_characterRigBody.velocity.x));
 
         LookAtOpponent(_rotationSpeed);
+
+    }
+    public void Jump()
+    {
+        
     }
     private void LookAtOpponent(float rotSpeed)
     {
         //getting the direction that should be looking at which is in this case the opponent
         Vector3 direction = _opponent.transform.position - transform.position;
-        //Then, get the dot product of the durection with the direction the opponent is looking 
+        //Then, get the dot product of the direction with the direction the opponent is looking 
         float dot = Vector3.Dot(direction, _opponent.transform.forward);
         //The result will be -1 if the target is directly behind, +1 if directly ahead and zero if it is side-on to the NPC in any direction (including above, below, etc). 
-
+        // the dot product is being used here to see the direction 1 if they are same direction, -1 if the exact opposite and zero if they are
+        //perpendicular
         if (dot <= 0)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 90, 0), Time.time * rotSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 90, 0),  rotSpeed);
             _oriantation = Oriantation.right;
             
         }
         else
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90, 0), Time.time * rotSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90, 0),  rotSpeed);
             _oriantation = Oriantation.left;
         }
 
     }
-
-    private void Update()
+ 
+    private void FixedUpdate()
     {
+       
         Move();
-        
+        Jump();
     }
 }
